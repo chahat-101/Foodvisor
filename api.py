14e055865aeca99df8a9dd15e8c29a390bbe323b
Chahat Patel <213402752+chahat-101@users.noreply.github.com>
2025-10-27 13:17:20 +0530

    Add cors midwr

--- api.py
+++ api.py
@@ -2,11 +2,21 @@ from dotenv import load_dotenv
 import os
 import base64
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from groq import Groq
 
 load_dotenv()
 api_key = os.getenv("GROQ_API_KEY")
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 client = Groq(api_key=api_key)
 
 
